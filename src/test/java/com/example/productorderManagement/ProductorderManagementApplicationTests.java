5f0972cdf5899443fbfa6204bbb869a891ad9ab4
zakiD9 <zakariadjerboa@gmail.com>
2025-08-16 16:29:04 +0100

    initial commit for the order management project

---
+++ src/test/java/com/example/productorderManagement/ProductorderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productorderManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductorderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
